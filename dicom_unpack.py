5d434c5fcad69513f129fdfdc06f4a84350e1af7
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2023-10-20 12:02:07 -0400

    Bug fix on proper pixel array slicing

--- dicom_unpack.py
+++ dicom_unpack.py
@@ -8,7 +8,7 @@ import pydicom as dicom
 import os
 import csv
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
 DISPLAY_TITLE = r"""
        _           _ _                                                   _    
@@ -85,7 +85,8 @@ def split_dicom_multiframe(dicom_data_set, output_file):
     os.makedirs(dir_path, exist_ok=True)
 
     for i, slice in enumerate(dicom_data_set.pixel_array):
-        dicom_data_set.PixelData = slice
+        dicom_data_set.PixelData = slice.tobytes()
+        dicom_data_set.NumberOfFrames = 1
         op_dcm_path = os.path.join(dir_path, f'slice_{i:03n}.dcm')
         print(f"Saving file : -->slice_{i:03n}.dcm<--")
         dicom_data_set.save_as(op_dcm_path)
